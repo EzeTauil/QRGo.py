2a893ce47787665f49654ad6baae53340354ce0c
Sh4d0w-X <118028611+EzeTauil@users.noreply.github.com>
2025-01-16 23:41:05 -0300

    Update QRGo.py

--- QRGo.py
+++ QRGo.py
@@ -102,8 +102,8 @@ menu_bar.add_cascade(label="Idioma", menu=menu_language)
 # Título
 # Crear el texto "sombra" desplazado y con un color oscuro
 shadow_label = tk.Label(root, text="Generador de QR", font=("Arial", 28, "bold"), 
-                        fg="gray", bg="white", relief="solid", padx=10, pady=10)
-shadow_label.place(x=240, y=15)  # Desplazamos un poco el texto de la sombra
+                        fg="gray",)
+shadow_label.place(x=230, y=15)  # Desplazamos un poco el texto de la sombra
 
 # Crear el texto principal
 label_title = tk.Label(root, text="Generador de QR", font=("Arial", 28, "bold"), 
